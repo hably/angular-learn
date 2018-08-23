9ad6a34a20017bb1f0ba3213ee0e164aebd08a44
hably <mohamedelhabib2001@yahoo.fr>
2018-08-23 10:45:59 +0200

    angular learning moodFace

--- ng-demo/src/app/app.module.ts
+++ ng-demo/src/app/app.module.ts
@@ -8,6 +8,12 @@ import { ReceipeComponent } from './receipe/receipe.component';
 import { FormComponent } from './form/form.component';
 import {FormsModule} from "@angular/forms";
 import { ConvertComponent } from './convert/convert.component';
+import { CanvasComponent } from './mood/canvas/canvas.component';
+import { TabListComponent } from './mood/tab-list/tab-list.component';
+import { MoodFaceComponent } from './mood/mood-face/mood-face.component';
+import { HappyComponent } from './mood/happy/happy.component';
+import { SosoComponent } from './mood/soso/soso.component';
+import { AngryComponent } from './mood/angry/angry.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { ConvertComponent } from './convert/convert.component';
     TemplateComponent,
     ReceipeComponent,
     FormComponent,
-    ConvertComponent
+    ConvertComponent,
+    CanvasComponent,
+    TabListComponent,
+    MoodFaceComponent,
+    HappyComponent,
+    SosoComponent,
+    AngryComponent
   ],
   imports: [
     BrowserModule, FormsModule
